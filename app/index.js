7f38718bbc1bd39af16bc2799fc5215419340747
Zulfat Ilyasov <mail@zulfat.net>
2016-04-28 15:14:43 +0300

    added titled box to exports

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ module.exports = {
   DefaultTheme: require('./themes/default'),
   Box: require('./components/box/box'),
   BoxTitle: require('./components/box/box-title/box-title'),
+  TitledBox: require('./components/box/titled-box/titled-box'),
   Table: require('./components/table/table'),
   TableRow: require('./components/table/table-row')
 };
