bef72582bec80021a568d3ba2acce8705b3a17ad
Zulfat Ilyasov <mail@zulfat.net>
2016-07-01 16:09:23 +0300

    removed shouldCompUpdate check from table row

--- app/components/table/table-row.jsx
+++ app/components/table/table-row.jsx
@@ -1,37 +1,6 @@
 var React = require('react');
 
-var areEqual = function(objA, objB){
-  if (objA === objB) {
-    return true;
-  }
-
-  if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) {
-    return false;
-  }
-
-  var keysA = Object.keys(objA);
-  var keysB = Object.keys(objB);
-
-  if (keysA.length !== keysB.length) {
-    return false;
-  }
-
-  // Test for A's keys different from B.
-  var bHasOwnProperty = hasOwnProperty.bind(objB);
-  for (var i = 0; i < keysA.length; i++) {
-    if (!bHasOwnProperty(keysA[i]) || (objA[keysA[i]] !== objB[keysA[i]] && typeof objB[keysA[i]] !== 'object')) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
 var TableRow = React.createClass({
-  shouldComponentUpdate(nextProps) {
-    return !areEqual(this.props.rowData, nextProps.rowData) || !areEqual(this.props.headers, nextProps.headers);
-  },
-
   render: function () {
     var tableCells = [];
     for (var prop in this.props.headers) {
